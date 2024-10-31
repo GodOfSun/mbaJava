4244e6d16b211ec7f744984bcbe5a8ebf8298cb6
Helio Formaggio <godofsun@gmail.com>
2024-10-31 20:21:28 -0300

    Repo

--- src/main/java/br/edu/infnet/projetoarqjavahelioformaggio/model/locationModel/Cidade.java
+++ src/main/java/br/edu/infnet/projetoarqjavahelioformaggio/model/locationModel/Cidade.java
@@ -1,14 +1,18 @@
 package br.edu.infnet.projetoarqjavahelioformaggio.model.locationModel;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import br.edu.infnet.projetoarqjavahelioformaggio.core.BaseEntity;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "cidade")
-public class Cidade {
+public class Cidade extends BaseEntity {
     private String nome;
+
+    @ManyToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "id_estado", nullable = false)
     private Uf estado;
+
     private String code;
 }
